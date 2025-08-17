72fff3962f9167c1f2ff59faa1f523dba4540b73
Andy Hall <alh17885@gmail.com>
2025-08-17 19:29:32 +0100

    fix: geist font now working with className, global scss import corrected

--- apps/site/src/app/dashboard/components/DialogTest.tsx
+++ apps/site/src/app/dashboard/components/DialogTest.tsx
@@ -4,20 +4,33 @@ import { useRef, useState } from "react";
 import styles from "../page.module.scss";
 import { InputText } from "@repo/ui/lib/molecules/inputs/input-text";
 import { Button } from "@repo/ui/lib/atoms/button";
+import { log } from "@repo/logger";
 
 export const DialogTest = () => {
   const dialogRef = useRef<HTMLDialogElement>(null);
+  const [text, setText] = useState('');
   const handleClick = () => {
+    log('showing dialog test')
     if (dialogRef.current) {
       dialogRef.current.showModal();
     }
   }
+  const handleClose = () => {
+    log('closing dialog test');
+    setText('');
+    dialogRef.current?.requestClose(text);
+  }
   return (
     <>
-      <dialog ref={dialogRef} className={styles.dialog} closedby="any">
+      <dialog
+        ref={dialogRef}
+        className={styles.dialog}
+        // @ts-expect-error: no def
+        closedby="any"
+      >
         <div style={{ background: "white", padding: "2rem", display: "flex", gap: "1rem" }}>
-          <InputText autoFocus />
-          <Button onClick={() => dialogRef.current?.requestClose()}>Close</Button>
+          <InputText autoFocus value={text} onChange={(ev) => setText(ev.target.value)} />
+          <Button onClick={handleClose}>Close</Button>
         </div>
       </dialog>
       <Button onClick={handleClick}>Open dialog</Button>
