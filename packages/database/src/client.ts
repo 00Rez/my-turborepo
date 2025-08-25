6eb318b2c54d6b4c41d7269efd72b638d68a2cd5
Andy Hall <alh17885@gmail.com>
2025-08-25 10:43:56 +0100

    chore(database): add TypeScript configuration file for the database package

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "../generated/prisma";
 //import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma || new PrismaClient();//.$extends(withAccelerate());
